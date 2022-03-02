b1226b760a97ee26b05708084e7758f776024bdd
Łukasz Szypliński <lukasz.szyplinski@gmail.com>
2022-03-02 22:02:27 +0100

    Favicon.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const MyDocument = () => (
   <Html>
     <Head>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <meta name="theme-color" content="#000000" />
+
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
